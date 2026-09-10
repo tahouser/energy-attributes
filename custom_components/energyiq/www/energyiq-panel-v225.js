bec0e4a98bb57dc90544e65e3bb1c72a9bcb6d4c
tahouser <124524637+tahouser@users.noreply.github.com>
2026-09-10 18:29:57 -0400

    Make v2.2.5 panel version label persistent

--- custom_components/energyiq/www/energyiq-panel-v225.js
+++ custom_components/energyiq/www/energyiq-panel-v225.js
@@ -7,6 +7,22 @@ const TAG = "energyiq-panel-v225";
 if (!customElements.get(TAG)) {
   const Base = customElements.get(BASE_TAG);
   if (!Base) throw new Error(`EnergyIQ base panel ${BASE_TAG} was not defined.`);
-  class EnergyIQPanelV225 extends Base {}
+  class EnergyIQPanelV225 extends Base {
+    _setVersionLabel() {
+      const sub = this.querySelector?.(".sub");
+      if (sub) sub.textContent = "Whole-home electrical intelligence · v2.2.5";
+    }
+    _render(...args) {
+      const result = super._render(...args);
+      this._setVersionLabel();
+      requestAnimationFrame(() => this._setVersionLabel());
+      return result;
+    }
+    connectedCallback() {
+      super.connectedCallback();
+      this._setVersionLabel();
+      requestAnimationFrame(() => this._setVersionLabel());
+    }
+  }
   customElements.define(TAG, EnergyIQPanelV225);
 }
