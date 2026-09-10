07d8cfe4a03d59abc36c2a211185774b05e60b1b
tahouser <124524637+tahouser@users.noreply.github.com>
2026-09-10 17:33:42 -0400

    Point EnergyIQ to v2.2.0 frontend

--- custom_components/energyiq/__init__.py
+++ custom_components/energyiq/__init__.py
@@ -31,8 +31,8 @@ async def async_setup(hass: HomeAssistant, config: dict) -> bool:
             hass=hass,
             frontend_url_path="energyiq",
             webcomponent_name="energyiq-panel-v209",
-            module_url=f"{URL_BASE}/energyiq-loader-v219.js",
-            sidebar_title="EnergyIQ • v2.1.9",
+            module_url=f"{URL_BASE}/energyiq-loader-v220.js",
+            sidebar_title="EnergyIQ • v2.2.0",
             sidebar_icon="mdi:lightning-bolt",
             require_admin=True,
         )
