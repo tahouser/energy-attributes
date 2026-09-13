9643bca185d8ab3e20ea2d9373991b7cfbba5797
tahouser <124524637+tahouser@users.noreply.github.com>
2026-09-12 20:11:49 -0400

    Simplify EnergyIQ constants for instrument foundation

--- custom_components/energyiq/const.py
+++ custom_components/energyiq/const.py
@@ -1,8 +1,3 @@
 """Constants for EnergyIQ."""
-
 DOMAIN = "energyiq"
 CONF_POWER_ENTITY = "power_entity"
-CONF_MONITORED_ENTITIES = "monitored_entities"
-
-TRAINING_SESSION = "training_session"
-TRAINED_DEVICES = "trained_devices"
