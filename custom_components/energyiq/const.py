7b49922f9d42aa0a001eb0cf51c4a50f34906198
tahouser <124524637+tahouser@users.noreply.github.com>
2026-09-12 21:19:12 -0400

    EnergyIQ v3.1.65 update source constants

--- custom_components/energyiq/const.py
+++ custom_components/energyiq/const.py
@@ -1,8 +1,7 @@
 """Constants for EnergyIQ."""
+from __future__ import annotations
 
 DOMAIN = "energyiq"
-CONF_POWER_ENTITY = "power_entity"
-CONF_MONITORED_ENTITIES = "monitored_entities"
-
-TRAINING_SESSION = "training_session"
-TRAINED_DEVICES = "trained_devices"
+CONF_HOST = "host"
+DEFAULT_HOST = "192.168.1.251"
+EM_ID = 0
