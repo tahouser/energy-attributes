3e58a63b1668b071de6489a035aee1d3e1e73d29
tahouser <124524637+tahouser@users.noreply.github.com>
2026-09-13 10:58:57 -0400

    Restore EnergyIQ entity-based constants

--- custom_components/energyiq/const.py
+++ custom_components/energyiq/const.py
@@ -6,8 +6,3 @@ CONF_MONITORED_ENTITIES = "monitored_entities"
 
 TRAINING_SESSION = "training_session"
 TRAINED_DEVICES = "trained_devices"
-
-# Shelly connection compatibility for the configuration flow.
-CONF_HOST = "host"
-DEFAULT_HOST = "192.168.1.251"
-EM_ID = 0
